e3c07c7ecadefda0a8dccd7488931a404ee80dbd
Nils Thamm <nils.thamm@student.hpi.de>
2018-10-24 17:28:52 +0800

    Step 1-4

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -15,25 +15,29 @@
 namespace opossum {
 
 void Chunk::add_segment(std::shared_ptr<BaseSegment> segment) {
-  // Implementation goes here
+  _segments.push_back(segment);
 }
 
 void Chunk::append(const std::vector<AllTypeVariant>& values) {
-  // Implementation goes here
+  Assert((values.size() == _segments.size()), "Column and value count must be the same");
+  for(ColumnID i = ColumnID{0}; i < values.size(); i++) {
+  	_segments[i]->append(values[i]);
+  }
 }
 
 std::shared_ptr<BaseSegment> Chunk::get_segment(ColumnID column_id) const {
-  // Implementation goes here
-  return nullptr;
+  return _segments[column_id];
 }
 
 uint16_t Chunk::column_count() const {
-  // Implementation goes here
-  return 0;
+  return _segments.size();
 }
 
 uint32_t Chunk::size() const {
-  // Implementation goes here
+  if (_segments.size())
+  {
+  	return _segments[0]->size();
+  }
   return 0;
 }
 
