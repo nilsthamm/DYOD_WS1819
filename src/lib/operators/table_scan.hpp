dc4e586c40401d2869f126061692fb4c60624188
Nils Thamm <nils.thamm@student.hpi.de>
2018-11-26 20:31:28 +0100

    Fixed compiler errors

--- src/lib/operators/table_scan.hpp
+++ src/lib/operators/table_scan.hpp
@@ -44,17 +44,24 @@ class TableScan : public AbstractOperator {
     TableScanImpl(ColumnID column_id, const ScanType scan_type,
             const AllTypeVariant search_value, std::shared_ptr<const Table> input_table ) : _column_id(column_id), _scan_type(scan_type), _search_value(type_cast<T>(search_value)), _input_table(input_table) {}
   
-  std::shared_ptr<const Table> _on_execute() {
+    std::shared_ptr<const Table> _on_execute() {
       auto output_table = std::make_shared<Table>(_input_table->chunk_size());
       auto pos_list = std::make_shared<PosList>();
 
-      for (auto chunk_id = ChunkID{0}; chunk_id < _input_table->chunk_out(); ++chunk_id) {
-        const auto& chunk = _input_table->get_chunk(chunk_id);
+      for (auto chunk_id = ChunkID{0}; chunk_id < _input_table->chunk_count(); ++chunk_id) {
+        // const auto& chunk = _input_table->get_chunk(chunk_id);
         for (auto column_id = ColumnID{0}; column_id < _input_table->column_count(); column_id++) {
         // Scan value segment
 
         // Scan reference segment
-          
+          // if(auto reference_segment = std::dynamic_pointer_cast<ReferenceSegment>(chunk.get_segment(column_id))) {
+          //   const auto referenced_pos_list = reference_segment->pos_list();
+          //   const auto reference_table = reference_segment->reference_table();
+
+          //   for (const auto row_id : referenced_pos_list) {
+
+          //   }
+          // }
 
         // Scan dictionary segment
 
