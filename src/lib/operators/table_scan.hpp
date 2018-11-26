bde7101473a48ffb3c4a2e029963f5bcb291cb7e
Nils Thamm <nils.thamm@student.hpi.de>
2018-11-26 22:25:04 +0100

    Table structure

--- src/lib/operators/table_scan.hpp
+++ src/lib/operators/table_scan.hpp
@@ -7,7 +7,10 @@
 
 #include "abstract_operator.hpp"
 #include "all_type_variant.hpp"
+#include "resolve_type.hpp"
+#include "storage/base_segment.hpp"
 #include "storage/dictionary_segment.hpp"
+#include "storage/reference_segment.hpp"
 #include "storage/table.hpp"
 #include "types.hpp"
 #include "type_cast.hpp"
@@ -68,7 +71,8 @@ class TableScan : public AbstractOperator {
     std::shared_ptr<const Table> _on_execute() {
       auto output_table = std::make_shared<Table>(_input_table->chunk_size());
       auto pos_list = std::make_shared<PosList>();
-      const auto comapre = compare_lambda(_scan_type); 
+      const auto compare = compare_lambda(_scan_type); 
+      const auto output_reference_table = _input_table;
 
       ValueID offset{0};
       for (auto chunk_id = ChunkID{0}; chunk_id < _input_table->chunk_count(); ++chunk_id) {
@@ -76,20 +80,25 @@ class TableScan : public AbstractOperator {
         // Scan value segment
 
         // Scan reference segment
-          // if(auto reference_segment = std::dynamic_pointer_cast<ReferenceSegment>(chunk.get_segment(_column_id))) {
-          //   const auto referenced_pos_list = reference_segment->pos_list();
-          //   const auto reference_table = reference_segment->reference_table();
+          if(auto reference_segment = std::dynamic_pointer_cast<ReferenceSegment>(chunk.get_segment(_column_id))) {
+            output_reference_table = reference_segment->reference_table();
+            const auto referenced_pos_list = reference_segment->pos_list();
+            const auto reference_table = reference_segment->reference_table();
 
-          //   for (const auto row_id : referenced_pos_list) {
-          //     if(auto value_segment = std::dynamic_pointer_cast<ValueSegment>(reference_table->get_chunk(row_id.chunk_id).get_segment(_column_id))) {
-          //       if()
-          //     } else if(auto dictionary = std::dynamic_pointer_cast<DictionarySegment>(reference_table->get_chunk(row_id.chunk_id).get_segment(_column_id))) {
-
-          //     } else {
-          //       Fail("Column type could not be reconized");
-          //     }
-          //   }
-          // }
+            for (const auto row_id : referenced_pos_list) {
+              if(auto value_segment = std::dynamic_pointer_cast<ValueSegment<T>>(reference_table->get_chunk(row_id.chunk_id).get_segment(_column_id))) {
+                if(compare(value_segment->values[row_id.chunk_offset])) {
+                  pos_list->emplace_back(row_id);
+                }
+              } else if(auto dictionary_segment = std::dynamic_pointer_cast<DictionarySegment<T>>(reference_table->get_chunk(row_id.chunk_id).get_segment(_column_id))) {
+                if(compare(dictionary_segment->get(row_id.chunk_offset))) {
+                  pos_list->emplace_back(row_id);
+                }
+              } else {
+                Fail("Column type could not be reconized");
+              }
+            }
+          }
         // Scan dictionary segment
         if (const auto dictionary_segment = std::dynamic_pointer_cast<DictionarySegment<T>>(chunk.get_segment(_column_id))) {
           const auto dictionary = dictionary_segment->dictionary();
@@ -113,6 +122,16 @@ class TableScan : public AbstractOperator {
         offset += chunk.size();
       }
       // Create table structure
+
+      Chunk output_chunk;
+
+      for(ColumnID column_id = ColumnID{0}; column_id < _input_table->column_count(); column_id++) {
+        const auto& column_type = _input_table->column_type(column_id);
+        output_table->add_column_definition(_input_table->column_name(column_id), column_type);
+        output_chunk.add_segment(make_shared_by_data_type<BaseSegment, ReferenceSegment>(column_type, output_reference_table, pos_list));
+      }
+
+      output_table->emplace_chunk(output_chunk);
 
       return output_table;
     }
--- src/lib/storage/table.cpp
+++ src/lib/storage/table.cpp
@@ -24,20 +24,21 @@ Table::Table(const uint32_t chunk_size) {
 }
 
 void Table::add_column_definition(const std::string& name, const std::string& type) {
-  // Implementation goes here
+  _column_names.push_back(name);
+  _column_types.push_back(type);
 }
 
 void Table::add_column(const std::string& name, const std::string& type) {
   _column_names.push_back(name);
   _column_types.push_back(type);
-  _chunks.back()->add_segment(make_shared_by_data_type<BaseSegment, ValueSegment>(type));
+  _chunks.back().add_segment(make_shared_by_data_type<BaseSegment, ValueSegment>(type));
 }
 
 void Table::append(std::vector<AllTypeVariant> values) {
-  if (_chunks.back()->size() == _chunk_size) {
+  if (_chunks.back().size() == _chunk_size) {
     _add_chunk();
   }
-  _chunks.back()->append(values);
+  _chunks.back().append(values);
 }
 
 void Table::create_new_chunk() {
@@ -48,8 +49,8 @@ uint16_t Table::column_count() const { return _column_types.size(); }
 
 uint64_t Table::row_count() const {
   uint64_t count = 0;
-  for (auto const chunk : _chunks) {
-    count += chunk->size();
+  for (const auto& chunk : _chunks) {
+    count += chunk.size();
   }
   return count;
 }
@@ -75,26 +76,26 @@ const std::string& Table::column_type(ColumnID column_id) const { return _column
 
 Chunk& Table::get_chunk(ChunkID chunk_id) {
   std::unique_lock<std::shared_mutex> lock(_mutex_chunk_access);
-  return *_chunks[chunk_id];
+  return _chunks[chunk_id];
 }
 
 const Chunk& Table::get_chunk(ChunkID chunk_id) const {
   std::shared_lock<std::shared_mutex> lock(_mutex_chunk_access);
-  return *_chunks[chunk_id];
+  return _chunks[chunk_id];
 }
 
 void Table::_add_chunk() {
-  auto const new_chunk = std::make_shared<Chunk>();
+  Chunk new_chunk;
   for (auto const& type : _column_types) {
-    new_chunk->add_segment(make_shared_by_data_type<BaseSegment, ValueSegment>(type));
+    new_chunk.add_segment(make_shared_by_data_type<BaseSegment, ValueSegment>(type));
   }
-  _chunks.push_back(new_chunk);
+  _chunks.emplace_back(std::move(new_chunk));
 }
 
 void Table::compress_chunk(ChunkID chunk_id) {
   Assert(chunk_id < _chunks.size(), "Chunk ID out of range");
 
-  const auto new_chunk = std::make_shared<Chunk>();
+  Chunk new_chunk;
 
   const auto& old_chunk = get_chunk(chunk_id);
 
@@ -103,15 +104,15 @@ void Table::compress_chunk(ChunkID chunk_id) {
   for (ColumnID column_id = ColumnID{0}; column_id < old_chunk.column_count(); ++column_id) {
     const auto segment = old_chunk.get_segment(column_id);
 
-    new_chunk->add_segment(make_shared_by_data_type<BaseSegment, DictionarySegment>(column_type(column_id), segment));
+    new_chunk.add_segment(make_shared_by_data_type<BaseSegment, DictionarySegment>(column_type(column_id), segment));
   }
 
   std::unique_lock<std::shared_mutex> lock(_mutex_chunk_access);
-  _chunks[chunk_id] = new_chunk;
+  _chunks[chunk_id] = std::move(new_chunk);
 }
 
-void emplace_chunk(Chunk chunk) {
-  // Implementation goes here
+void Table::emplace_chunk(Chunk& chunk) {
+  _chunks.emplace_back(std::move(chunk));
 }
 
 }  // namespace opossum
