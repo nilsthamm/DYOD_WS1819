9331451a766599cc4b72678f5d1b0b001ee312ae
m0r13 <moritz.hilscher@gmail.com>
2018-11-26 20:57:39 +0100

    Start handling dictionary segment in table scan.

--- src/lib/operators/table_scan.hpp
+++ src/lib/operators/table_scan.hpp
@@ -7,6 +7,7 @@
 
 #include "abstract_operator.hpp"
 #include "all_type_variant.hpp"
+#include "storage/dictionary_segment.hpp"
 #include "storage/table.hpp"
 #include "types.hpp"
 #include "type_cast.hpp"
@@ -48,26 +49,36 @@ class TableScan : public AbstractOperator {
       auto output_table = std::make_shared<Table>(_input_table->chunk_size());
       auto pos_list = std::make_shared<PosList>();
 
+      ValueID offset{0};
       for (auto chunk_id = ChunkID{0}; chunk_id < _input_table->chunk_count(); ++chunk_id) {
-        // const auto& chunk = _input_table->get_chunk(chunk_id);
-        for (auto column_id = ColumnID{0}; column_id < _input_table->column_count(); column_id++) {
+        const auto& chunk = _input_table->get_chunk(chunk_id);
         // Scan value segment
 
         // Scan reference segment
-          // if(auto reference_segment = std::dynamic_pointer_cast<ReferenceSegment>(chunk.get_segment(column_id))) {
-          //   const auto referenced_pos_list = reference_segment->pos_list();
-          //   const auto reference_table = reference_segment->reference_table();
-
-          //   for (const auto row_id : referenced_pos_list) {
-
-          //   }
-          // }
-
+          
         // Scan dictionary segment
+        if (const auto dictionary_segment = std::dynamic_pointer_cast<DictionarySegment<T>>(chunk.get_segment(_column_id))) {
+          const auto dictionary = dictionary_segment->dictionary();
+          const auto attribute_vector = dictionary_segment->attribute_vector();
 
+          std::vector<bool> contained_values(chunk.size(), false);
+          for (size_t index = 0; index < contained_values.size(); ++index) {
+            bool contained = false;
+            // TODO perform comparation!
+            contained_values[i] = contained;
+          }
+
+          for (size_t index = 0; index < attribute_vector->size(); ++index) {
+            ValueID value_id = attribute_vector->get(index);
+            if (contained_values[value_id]) {
+              pos_list->emplace_back(offset + value_id);
+            }
+          }
         }
+
+        offset += chunk.size();
       }
-    // Create table structure
+      // Create table structure
 
       return output_table;
     }
