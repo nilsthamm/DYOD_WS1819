4843d66e81f027ae5683c517111d5727fcada592
Nils Thamm <nils.thamm@student.hpi.de>
2018-11-27 22:58:22 +0100

    Changed naming

--- src/lib/operators/table_scan.cpp
+++ src/lib/operators/table_scan.cpp
@@ -17,7 +17,7 @@ namespace opossum {
   const AllTypeVariant& TableScan::search_value() const { return _search_value; }
 
   std::shared_ptr<const Table> TableScan::_on_execute() { 
-    return _table_scan_impl->_on_execute();
+    return _table_scan_impl->on_execute();
   }
 
 } // namespace opossum
